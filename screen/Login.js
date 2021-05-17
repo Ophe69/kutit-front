0a2f9e230c6f531aff74e7719e596a9ccfa800a8
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-05-17 14:20:45 +0200

    first modif on login page

--- screen/Login.js
+++ screen/Login.js
@@ -1,22 +1,78 @@
-import React from 'react'
-import { StyleSheet, Text, View} from 'react-native';
-import { Button } from 'react-native-elements';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, ImageBackground} from 'react-native';
+import { Button, Input, inputStyle, InputText, CheckBox } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { connect } from 'react-redux';
 
 
 export default function Login(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [checked, setChecked] = useState(false);
+
+    
     return (
-        <View style={styles.container}>
-            <Text>Login</Text>
+        <ImageBackground /* source={require('../assets/images/pexels-thgusstavo-santana-2076932.jpg')} */ style={styles.container}>
+            <Input
+            value={email}
+            placeholder='email'
+            leftIcon={{ type: 'font-awesome', name: 'user', color: 'black'}}
+            containerStyle={{ marginBottom: 25, width: '70%' }}
+            inputStyle={{ marginLeft: 10 }}
+            
+            />
+
+            <Input
+            value={password}
+            placeholder='password'
+            leftIcon={{ type: 'font-awesome', name: 'lock', color: 'black' }}
+            containerStyle={{ marginBottom: 25, width: '70%', color: 'black' }}
+            inputContainerStyle={{color: 'black'}}
+            inputStyle={{ marginLeft: 10}}
+            />
+    
+            
             <Button
-                title='Get an haircut'
-                onPress={() => {
+                title='Se connecter'
+                buttonStyle={{backgroundColor: '#354F52'}}
+/*                 onPress={() => {
                     props.navigation.navigate('BottomNavigator', { screen: 'Home' });
+                }{e => {setEmail(e.target.value); console.log('email')}}
+            } */
+            />
+
+            <CheckBox style={{backgroundColor: ''}}
+            center
+            title='Accepter les CGV'
+            /* checked={state.checked}
+            onPress={() => this.setState({checked: !this.state.checked})} */
+            />
+
+            <Button
+                title='Créer un compte'
+                buttonStyle={{backgroundColor: '#354F52'}}
+                onPress={() => {
+                    props.navigation.navigate('BottomNavigator', { screen: 'Profile' });
                 }}
             />
-        </View>
+
+            <Button
+            type="outline"
+            icon={
+                <Icon style={{ marginRight: 20, marginLeft: 20, color: "blue" }}
+                name="facebook"
+                size={15}
+                color="white"
+                
+                />
+            }
+            title="Se connecter avec Facebook"
+            />
+        </ImageBackground>
         
     )
-}
+            }
 
 
 const styles = StyleSheet.create({
@@ -25,5 +81,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        
     },
+    background: {
+        color: 'white'
+    }
 });
